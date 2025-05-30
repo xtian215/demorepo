d3041eb19d5c409258d39f17b61efea194dbc453
xtian215 <christiannicole.delfin@cvsu.edu.ph>
2025-05-30 13:53:44 +0800

    Login Commit

--- Loginpage/loginpage.js
+++ Loginpage/loginpage.js
@@ -1,4 +1,5 @@
 console.log('LoginPage Version 1');
-<<<<<<< HEAD
 console.log('LoginPage Version 2');
 console.log('LoginPage Version 3');
+console.log('LoginPage Version 4');
+console.log('LoginPage Version 5');
